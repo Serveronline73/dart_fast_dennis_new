c3d72de18f4193fbf94b65a1296c8d6c125551de
dennis <info.dennis.durmus@gmail.com>
2024-10-07 09:52:13 +0200

    intial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dart_fast_new
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/dart_fast_new/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dart_fast_new
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
